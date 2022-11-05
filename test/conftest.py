15a95fbde47426a9715d331f19f30365a12791be
Andrew Herrington <andrew.the.techie@gmail.com>
2022-11-05 01:09:01 -0500

    ci: some working tests

---
+++ test/conftest.py
@@ -0,0 +1,58 @@
+import os
+import random
+import string
+import time
+
+import docker
+import pytest
+import pytest_asyncio
+from aiobotocore.session import AioSession
+from aiobotocore.session import get_session
+from pydantic_sqs import SQSQueue
+
+
+@pytest_asyncio.fixture(scope="session", name="localstack_container")
+def create_localstack_container(unused_tcp_port_factory):
+    """Runs a localstack container for use with testing"""
+    port1 = unused_tcp_port_factory()
+    port2 = unused_tcp_port_factory()
+    try:
+        client = docker.from_env()
+        container = client.containers.run(
+            f"localstack/localstack:{os.environ.get('LOCALSTACK_IMAGE_TAG', 'latest')}",
+            ports={"4566/tcp": port1, "4571/tcp": port2},
+            detach=True,
+        )
+        yield f"http://localhost:{port1}/", port2
+    finally:
+        container.remove(force=True)
+
+
+@pytest_asyncio.fixture(name="localstack_sqs_client_args")
+async def create_localstack_sqs_client(localstack_container):
+    endpoint_url = localstack_container[0]
+    session = get_session()
+    client_kwargs = {
+        "region_name": "us-east-1",
+        "use_ssl": False,
+        "endpoint_url": endpoint_url,
+    }
+    yield session, client_kwargs
+
+
+@pytest_asyncio.fixture(name="localstack_queue")
+async def create_localstack_queue(localstack_sqs_client_args):
+    """Creates a queue in localstack for use with testing"""
+    session = localstack_sqs_client_args[0]
+    client_kwargs = localstack_sqs_client_args[1]
+    queue_name = "".join(
+        random.choices(string.ascii_uppercase + string.ascii_lowercase, k=8)
+    )
+    async with session.create_client("sqs", **client_kwargs) as client:
+        response = await client.create_queue(QueueName=queue_name)
+        queue = SQSQueue(
+            response["QueueUrl"],
+            endpoint_url=client_kwargs["endpoint_url"],
+            use_ssl=False,
+        )
+        yield queue, session, client_kwargs
--- test/test_pydantic_sqs.py
+++ test/test_pydantic_sqs.py
@@ -1,5 +1,89 @@
 import pytest
+import pytest_asyncio
+from aiobotocore.session import get_session
+from pydantic_sqs import SQSModel
+from pydantic_sqs import SQSQueue
 
 
-def test_fake():
-    assert True
+class ThisModel(SQSModel):
+    test: str
+
+
+class ThatModel(SQSModel):
+    foo: str
+
+
+@pytest.mark.asyncio
+async def test_send_to_queue(localstack_queue):
+    queue = localstack_queue[0]
+    session = localstack_queue[1]
+    client_kwargs = localstack_queue[2]
+    queue.register_model(ThisModel)
+    test = ThisModel(test="test")
+    await test.to_sqs()
+    assert test.message_id is not None
+    async with session.create_client("sqs", **client_kwargs) as client:
+        response = await client.get_queue_attributes(
+            QueueUrl=queue.queue_url, AttributeNames=["ApproximateNumberOfMessages"]
+        )
+    assert int(response["Attributes"]["ApproximateNumberOfMessages"]) > 0
+
+
+@pytest.mark.asyncio
+async def test_from_queue(localstack_queue):
+    queue = localstack_queue[0]
+    queue.register_model(ThisModel)
+    test = ThisModel(test="test")
+    await test.to_sqs()
+    assert test.message_id is not None
+
+    from_sqs = await queue.from_sqs()
+    assert len(from_sqs) == 1
+    assert from_sqs[0].test == "test"
+    assert from_sqs[0].message_id == test.message_id
+    assert from_sqs[0].receipt_handle is not None
+    assert from_sqs[0].deleted is False
+
+
+@pytest.mark.asyncio
+async def test_delete(localstack_queue):
+    queue = localstack_queue[0]
+    session = localstack_queue[1]
+    client_kwargs = localstack_queue[2]
+    queue.register_model(ThisModel)
+    test = ThisModel(test="test")
+    await test.to_sqs()
+    assert test.message_id is not None
+
+    from_sqs = await queue.from_sqs()
+    await from_sqs[0].delete_from_queue()
+    assert from_sqs[0].deleted is True
+
+    async with session.create_client("sqs", **client_kwargs) as client:
+        response = await client.get_queue_attributes(
+            QueueUrl=queue.queue_url, AttributeNames=["ApproximateNumberOfMessages"]
+        )
+    assert int(response["Attributes"]["ApproximateNumberOfMessages"]) == 0
+
+
+@pytest.mark.asyncio
+async def test_multiple_models_in_queue(localstack_queue):
+    queue = localstack_queue[0]
+    session = localstack_queue[1]
+    client_kwargs = localstack_queue[2]
+    queue.register_model(ThisModel)
+    queue.register_model(ThatModel)
+    test = ThisModel(test="test")
+    test2 = ThatModel(foo="bar")
+    await test.to_sqs()
+    await test2.to_sqs()
+    assert test.message_id is not None
+    assert test2.message_id is not None
+    async with session.create_client("sqs", **client_kwargs) as client:
+        response = await client.get_queue_attributes(
+            QueueUrl=queue.queue_url, AttributeNames=["ApproximateNumberOfMessages"]
+        )
+    assert int(response["Attributes"]["ApproximateNumberOfMessages"]) == 2
+
+    from_sqs = await queue.from_sqs(max_messages=5)
+    assert len(from_sqs) == 2
